655fa02cac0ed2a5a925cbceda6066617054af3f
itsAbhi650 <abhinavp650@gmail.com>
2022-04-07 07:59:00 +0530

    Initial Commit

---
+++ RandomSerialData/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace RandomSerialData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(3, 263);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 296);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Close RealTerm for a timeout event";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(259, 257);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 309);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "RandomSerialData";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ RandomSerialData/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace RandomSerialData
+{
+    public partial class Form1 : Form
+    {
+        byte[] buffer = new byte[1024];
+        CancellationTokenSource tokenSource;
+        SerialPort Port;
+        public Form1()
+        {
+            InitializeComponent();
+            Port = new SerialPort("COM3", 9600)
+            {
+                StopBits = StopBits.Two,
+                DataBits = 8,
+                Parity = Parity.None,
+                DiscardNull = true,
+                WriteTimeout = 2000
+            };
+            Port.Open();
+            Port.DataReceived += Port_DataReceived;
+        }
+
+        private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            textBox1.AppendText($"Data received{Environment.NewLine}");
+        }
+
+        async Task GenerateRandomAsync()
+        {
+            Random rand = new Random(255);
+            while (!tokenSource.IsCancellationRequested)
+            {
+                await Task.Run(() =>
+                {
+                    for (int i = 0; i < 1024; i++)
+                    {
+                        buffer[i] = (byte)rand.Next(0, 255);
+                    }
+                    try
+                    {
+                        Port.Write(buffer, 0, buffer.Length);
+                        textBox1.AppendText($"buffer[1024] written at port {Port.PortName}.{Environment.NewLine}");
+                    }
+                    catch (TimeoutException ex)
+                    {
+                        textBox1.AppendText(ex.Message+Environment.NewLine);
+                        textBox1.AppendText($"Make sure device is connected!{Environment.NewLine}");
+                        tokenSource.Cancel();
+                        button1.Text = "Start";
+                    }
+                    finally
+                    {
+                        textBox1.ScrollToCaret();
+                    }
+                });
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn.Text == "Start")
+            {
+                btn.Text = "Stop";
+                textBox1.Clear();
+                tokenSource = new CancellationTokenSource();
+                GenerateRandomAsync();
+            }
+            else
+            {
+                tokenSource.Cancel();
+                btn.Text = "Start";
+            }
+        }
+    }
+}
